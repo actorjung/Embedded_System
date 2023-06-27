2be8cfd4379dcdc63402c996177d147c5f93011c
woosung <jwos0705@gmail.com>
2023-06-27 14:36:11 +0900

    Embedded_System

---
+++ PROJECT/clock.h
@@ -0,0 +1,78 @@
+// clock.h
+#include <cstdint>
+#include "mbed.h"
+#include "pitches.h"
+#include "C12832.h"
+
+struct CurrentTime {
+    uint8_t HH = 0;
+    uint8_t MM = 0;
+    uint8_t SS = 0;
+};
+
+struct Alarm1 {
+    uint8_t HH = 0;
+    uint8_t MM = 0;
+    uint8_t SS = 0;
+    bool alarmSet = false;
+};
+
+struct Alarm2 {
+    uint8_t HH = 0;
+    uint8_t MM = 0;
+    uint8_t SS = 0;
+    bool alarmSet = false;
+};
+
+struct Alarm3 {
+    uint8_t HH = 0;
+    uint8_t MM = 0;
+    uint8_t SS = 0;
+    bool alarmSet = false;
+};
+
+extern uint8_t screenMode; // lcd에 출력될 화면을 선택
+extern bool setClockMode; // 시간 설정 모드 on(=true), off()
+extern bool lcdCleared;
+
+extern CurrentTime c1;
+extern Alarm1 a1;
+extern Alarm2 a2;
+extern Alarm3 a3;
+
+extern float frequency[];
+extern float beat[];
+
+extern InterruptIn up; // up = A2
+extern InterruptIn down; // down = A3
+extern InterruptIn left; // left = A4
+extern InterruptIn rite; // rite = A5
+extern InterruptIn center; // center = D4
+extern InterruptIn sw1; // sw1 = D3
+extern InterruptIn sw2; // sw2 = D8
+extern InterruptIn sw3; // sw3 = D9
+extern DigitalOut led_r; // led_r = D5
+extern C12832 lcd; // lcd = D11, D13, D12, D7, D10
+extern PwmOut buzzer; // buzzer = PTA1
+
+extern bool setAlarm1; // 알람1 : true == set, false == unset
+extern bool setAlarm2; // 알람2 : true == set, false == unset
+extern bool setAlarm3; // 알람3 : ture == set, false == unset
+extern bool ringAlarm; // 스피커에서 알람이 울리고 있으면 true, 울리지 않고 있으면 false 
+extern uint8_t snoozeState;
+
+void up_HH(); // ScreenMode에 따라 HH 1 증가
+void down_HH(); // ScreenMode에 따라 HH 1 감소
+void up_MM(); // ScreenMode에 따라 MM 1 증가
+void down_MM(); // ScreenMode에 따라 MM 1 감소
+void reset_SS(); // MainScreen에서만 사용, SS 0으로 초기화
+void buzzer_trigger(); // buzzer를 울림
+void led_trigger(); // LED를 점멸함
+void print_main_screen(); // main화면 lcd에 출력
+void print_set_alarm1(); // alarm설정1 lcd에 출력
+void print_set_alarm2(); // alarm설정2 lcd에 출력
+void print_set_alarm3(); // alarm설정3 lcd에 출력
+void snooze(); // 알람이 울리고 있을 때 누르면 5분 뒤에 다시 울림
+
+
+
---
+++ PROJECT/thread.h
@@ -0,0 +1,5 @@
+// thread.h
+void print_screen();
+void up_time();
+void make_sound();
+void make_light();
